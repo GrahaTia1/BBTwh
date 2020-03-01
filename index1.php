4a2c697875e3d77f3fc5a948afe9adeaa5346132
GrahaTia1 <61170099+GrahaTia1@users.noreply.github.com>
2020-03-01 20:18:36 +0800

    Update index1.php

--- index1.php
+++ index1.php
@@ -18,8 +18,7 @@ echo "浏览量：". $_SESSION['views'];
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>NIPPON</title>
-    <link rel="icon" href="images/NIPPON.ico">
+    <title>进来就昏睡x</title>
     <link rel="stylesheet" type="text/css" href="css/index.css">
     <link rel="stylesheet" type="text/css" href="div.css" charset="utf-8">
     <script type="text/javascript">
